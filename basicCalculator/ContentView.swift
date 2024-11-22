02783204e9c4bba1cbb26a3d9fadbd98b3cc64f1
Onur <onur.ozdogan@kartalbombe.com.tr>
2024-11-22 14:48:02 +0300

    Initial Commit

---
+++ basicCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  basicCalculator
+//
+//  Created by Onur Özdoğan on 22.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ basicCalculator/basicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicCalculatorApp.swift
+//  basicCalculator
+//
+//  Created by Onur Özdoğan on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct basicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
